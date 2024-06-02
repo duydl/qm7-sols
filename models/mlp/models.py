3b6bab03f27be640e4dddf4f8e11c3ec16a978af
duydl <duy.dole.00ece@gmail.com>
2024-06-02 10:24:07 +0700

    Allow custom noise in RandomSortCM

--- models/mlp/models.py
+++ models/mlp/models.py
@@ -7,32 +7,30 @@ import pytorch_lightning as pl
 
 from torchmetrics import MeanAbsoluteError, MeanSquaredError
 
-class CoulombPreprocess(nn.Module):
-    def __init__(self, matrix_size, step=1.0, noise=1.0):
-        super(CoulombPreprocess, self).__init__()
-        self.step = step
-        self.noise = noise
+class RandomSortCM(nn.Module):
+    def __init__(self, matrix_size):
+        super(RandomSortCM, self).__init__()
         self.triuind = torch.triu_indices(matrix_size, matrix_size, offset=0).t().contiguous().view(-1)
 
-    def realize(self, X):
+    def realize(self, X, noise):
         X_flat = []
         for x in X:
             norms = torch.norm(x, dim=1)
-            inds = torch.argsort(-norms + torch.randn_like(norms) * self.noise)
+            inds = torch.argsort(-norms + torch.randn_like(norms) * noise)
             x_sorted = x[inds, :][:, inds]
             x_flattened = x_sorted.flatten()[self.triuind]
             X_flat.append(x_flattened)
         return torch.stack(X_flat)
 
-    def forward(self, X):
-        return self.realize(X)
+    def forward(self, X, noise):
+        return self.realize(X, noise)
     
 
 class MLP(nn.Module):
     def __init__(self, matrix_size, output_size, hidden_sizes=[400, 100], activation_type="tanh"):
         super(MLP, self).__init__()
         
-        self.preprocess = CoulombPreprocess(matrix_size)
+        self.preprocess = RandomSortCM(matrix_size)
         input_size = self.preprocess.triuind.size(0)
         
         # Create a list of all sizes: input, hidden layers, output
@@ -52,13 +50,13 @@ class MLP(nn.Module):
         # Unwrap into nn.Sequential
         self.network = nn.Sequential(*layers)
 
-    def forward(self, X):
-        processed_X = self.preprocess(X)
+    def forward(self, X, noise=1.0):
+        processed_X = self.preprocess(X, noise)
         return self.network(processed_X)
 
 
 class ModelPL(pl.LightningModule):
-    def __init__(self, model, learning_rate=0.01, batch_size=64):
+    def __init__(self, model, learning_rate=0.001, batch_size=64):
         super().__init__()
         self.model = model
         self.learning_rate = learning_rate
@@ -75,33 +73,31 @@ class ModelPL(pl.LightningModule):
         self.val_mse = MeanSquaredError()
         self.test_mse = MeanSquaredError()
 
-    def forward(self, data):
-        return self.model(data)
+    def forward(self, data, noise=1.0):
+        return self.model(data, noise=noise)
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        # lr_scheduler = {
-        #     'scheduler': optim.lr_scheduler.ReduceLROnPlateau(
-        #         optimizer, 
-        #         mode='min', 
-        #         factor=0.25, 
-        #         patience=1),
-        #     'monitor': 'val_loss', 
-        #     'interval': 'epoch',
-        #     'frequency': 1
-        # }
-        def lr_lambda(epoch):
-            if epoch > 12500:
-                return 0.01
-            elif epoch > 2500:
-                return 0.005
-            elif epoch > 500:
-                return 0.0025
-            else:
-                return 0.001
-
-        # Using LambdaLR to adjust learning rate based on epoch
-        lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+        lr_scheduler = {
+            'scheduler': optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, 
+                mode='min', 
+                factor=0.25, 
+                patience=5),
+            'monitor': 'val_loss', 
+            'interval': 'epoch',
+            'frequency': 1
+        }
+        # def lr_lambda(epoch):
+        #     if epoch > 12500:
+        #         return 0.01
+        #     elif epoch > 2500:
+        #         return 0.005
+        #     elif epoch > 500:
+        #         return 0.0025
+        #     else:
+        #         return 0.001
+        # lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
         return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler}
 
@@ -109,24 +105,21 @@ class ModelPL(pl.LightningModule):
         inputs, targets = data[0], data[1]
         outputs = self(inputs)
         loss = self.criterion(outputs, targets)
-        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
+        # self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
         
-        self.train_mae(outputs, targets)
-        self.log("train_mae", self.train_mae, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
+        # self.train_mae(outputs, targets)
+        # self.log("train_mae", self.train_mae, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
         
-        self.train_mse(outputs, targets)
-        self.log('train_mse', self.train_mse, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
+        # self.train_mse(outputs, targets)
+        # self.log('train_mse', self.train_mse, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
         
         return loss
 
     def validation_step(self, data, batch_idx):
         inputs, targets = data[0], data[1]
-        outputs = self(inputs)
+        outputs = self(inputs, noise=0.0)
         loss = self.criterion(outputs, targets)
         self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
-        
-        self.val_mae(outputs, targets)
-        self.log("val_mae", self.val_mae, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
         
         self.val_mse(outputs, targets)
         self.log('val_mse', self.val_mse, on_step=False, on_epoch=True, prog_bar=True, batch_size=self.batch_size)
