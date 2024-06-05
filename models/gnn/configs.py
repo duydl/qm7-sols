57dbf885d77de89bc10d70299ee7aee0e4a28ab3
duydl <duy.dole.00ece@gmail.com>
2024-06-06 04:02:21 +0700

    [gnn] [mlp] Add hyperparam configs

---
+++ models/gnn/configs.py
@@ -0,0 +1,28 @@
+HYPERPARAMS = {
+    "CustomGNN_1": {
+        "hidden_dim": 128,
+        "output_dim": 1,
+        "node_features": 1,
+        "pos_dim": 3,
+        "edge_dim": 0,
+    },
+    "CustomGNN_2": {
+        "hidden_dim": 128,
+        "output_dim": 1,
+        "node_features": 1,
+        "pos_dim": 3,
+        "edge_dim": 0,
+        "num_filters": 8
+    },
+    "CustomGNN_3": {
+        "hidden_dim": 128,
+        "output_dim": 1,
+        "node_features": 1,
+        "pos_dim": 3,
+        "edge_dim": 0,
+        "interaction_nums": 5
+    },
+    "DimeNet": {
+        
+    }
+}
